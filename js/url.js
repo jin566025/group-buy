1b00aa5fd354aea2f16690571a31632dd8086db7
jin566025 <jct@myxinhai.cn>
2018-01-09 08:17:28 +0800

    新增库存数量显示

--- js/url.js
+++ js/url.js
@@ -1,2 +1,7 @@
 var url_path = "https://dxh.xinhaiip.cn/"
 var member_id = sessionStorage.getItem("member_id")
+
+var pay_url = "http://wcphp172.xinhaimobile.cn/ixinhai_pay/pay/group_pay.php"
+var return_url = "http://wcphp172.xinhaimobile.cn/xh_internal_bulk/history.html"
+$("#myForm,.myForm").attr("action",pay_url)
+$("#return_url,.return_url").attr("action",return_url)
